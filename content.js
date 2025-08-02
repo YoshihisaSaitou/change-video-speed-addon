ca0fe4a17c5c40418801def2f0738ac8285fbebb
Yoshihisa Saito <anti_viral_drug@yahoo.co.jp>
2025-08-02 19:44:42 +0900

    Switch hotkey to Shift+G

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 document.addEventListener("keydown", (e) => {
-  if (e.ctrlKey && e.key.toLowerCase() === "g") {
+  if (e.shiftKey && e.key.toLowerCase() === "g") {
     e.preventDefault();
     document.querySelectorAll("video").forEach((video) => {
       video.playbackRate = 2.3;
